ef284c9e8843a87577742cf3aba4f0eeb4f47ecd
MortonnnE <jackster95@hotmail.com>
2020-04-17 11:25:31 +1200

    Changed comment

--- MyFirstProject/src/HelloWordExample.java
+++ MyFirstProject/src/HelloWordExample.java
@@ -2,7 +2,7 @@
 public class HelloWordExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Output Hello WOrld to the user
 		System.out.println("Hello World");
 
 	}
